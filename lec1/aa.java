ff04ab46a12d4792b37ef249fb92ba8350fa347e
Shu Liu <sliu2200899@gmail.comm>
2018-03-29 00:18:14 -0400

    modify

--- lec1/aa.java
+++ lec1/aa.java
@@ -2,5 +2,6 @@ public class aa {
     public static void main(String [] args) {
         int a = (int)Math.pow(2,4);
         System.out.println("the value of a is" + a);
+	System.out.println("the setting is done.");
     }
 }
